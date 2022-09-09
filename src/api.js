0e1e65b4f922beaba24f30162bc926e787367a07
Hung Dinh Nguyen <hung.dinh.nguyen.b@gmail.com>
2022-09-08 22:38:00 -0400

    Fetch JSON data from GeoDB Cities API

---
+++ src/api.js
@@ -0,0 +1,9 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'e5c10403afmsh0bfa60ab57612bcp1ca6e6jsn9b4011882a9c',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo';
