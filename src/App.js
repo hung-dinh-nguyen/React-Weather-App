0adf1b6dd394d733f2a2a76c9a61c0e4529674a5
Hung Dinh Nguyen <hung.dinh.nguyen.b@gmail.com>
2022-09-09 21:18:34 -0400

    Save Point
    Completed the currentWeather component, but console is logging an error

--- src/App.js
+++ src/App.js
@@ -1,12 +1,36 @@
 import './App.css';
+import { useState } from "react";
 import Search from './components/search';
 import CurrentWeather from './components/current-weather/current-weather';
+import {WEATHER_API_KEY, WEATHER_API_URL} from './api';
 
 function App() {
+  const [currentWeather, setCurrentWeather] = useState(null);
+  const [forecast, setForecast] = useState(null);
 
   const handleOnSearchChange = (searchData) => {
-    console.log(searchData);
-  }
+    const [lat, lon] = searchData.value.split(" ");
+
+    const currentWeatherFetch = fetch(
+      `${WEATHER_API_URL}/weather?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}&units=metric`
+    );
+    const forecastFetch = fetch(
+      `${WEATHER_API_URL}/forecast?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}&units=metric`
+    );
+
+    Promise.all([currentWeatherFetch, forecastFetch])
+      .then(async (response) => {
+        const weatherResponse = await response[0].json();
+        const forcastResponse = await response[1].json();
+
+        setCurrentWeather({ city: searchData.label, ...weatherResponse });
+        setForecast({ city: searchData.label, ...forcastResponse });
+      })
+      .catch(console.log);
+  };
+
+  console.log(currentWeather);
+  console.log(forecast);
 
   return (
     <div className="container">
